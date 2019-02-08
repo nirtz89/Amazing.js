3d7cd4e0c2d7d83c2ae26d17e3506c88712181a6
Nir Tzezana <nirtz89@gmail.com>
2019-02-08 20:22:38 +0200

    General fixes and MD file

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Amazing.js
-Elements animation reveal when scrolled over
+## Amazing.js
+
+Reveal animation on scrolled-over elements, easy as that!
+
+## Quick Start
+
+### Place your elements in the DOM
+```html
+<h1>Amazing.js</h1>
+```
+
+### Add required proprties
+```html
+<h1 class="amazing" data-animation="fadeIn">Amazing.js</h1>
+```
+
+### Create and initialize an Amazing.js object
+```javascript
+const amazing = new Amazing();
+amazing.init();
+```
+
+### Watch the magic happens
+![](https://i.imgur.com/6wPl4zH.gif)
+
+## Features
+
+* **NO jQuery** - Vanilla it is.
+* Pick from a variety of animations (all from the awesome Animate.css).
+* Set delay, speed and animation for each object independently.
+* Or create these by default when creating the Amazing.js object.
+* Tiny.
+
+
+## Getting Started
+
+### Install On your local PC
+
+#### Download Sources
+
+use npm
+
+```bash
+npm install amazingjs
+```
+
+use git
+
+```bash
+git clone https://github.com/nirtz89/Amazing.js.git
+```
--- src/script.js
+++ src/script.js
@@ -6,57 +6,55 @@ class Amazing {
         this.config.useClass = this.config.useClass || "amazing";
         this.config.defaultAnimation = this.config.defaultAnimation || false;
         this.config.defaultSpeed = this.config.defaultSpeed || false;
+        this.config.defaultDelay = this.config.defaultDelay || 0;
         this.debug = debug;
     }
 
     init() {
-        var self = this;
+        let self = this;
         if (self.debug)
             // If in debug mode, state the library has initiated and the object has been created
             console.log("Amazing initiated");
 
         function isScrolledIntoView(el) {
             // If elements is scrolled into view
-            var rect = el.getBoundingClientRect();
-            var elemTop = rect.top;
-            var elemBottom = rect.bottom;
-            var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+            let rect = el.getBoundingClientRect();
+            let elemTop = rect.top;
+            let elemBottom = rect.bottom;
+            let isVisible = elemTop < window.innerHeight && elemBottom >= 0;
             return isVisible;
         }
 
         function setAnimation(el) {
-            var animation = el.dataset.animation || self.config.defaultAnimation;
-            var delay = parseFloat(el.dataset.delay) || 0;
-            var speed = parseFloat(el.dataset.speed) || 1;        
-            var after = el.dataset.after || false;
+            let animation = el.dataset.animation || self.config.defaultAnimation;
+            let delay = parseFloat(el.dataset.delay) || self.config.defaultDelay;
+            let speed = parseFloat(el.dataset.speed) || self.config.defaultSpeed;
             setTimeout(()=>{
                 el.classList.add("finished");
                 // Find elements waiting for this to finish and activate animation on them
-                var amazingClass = el.className.split(" ").filter((v)=>v.indexOf("amazing-")>-1)[0].replace("amazing-","");
-                var allAfter = document.querySelectorAll(`[data-after="${amazingClass}"]`);
-                allAfter.forEach((after_element) => {
-                    setAnimation(after_element);
-                });
+                if (el.className.indexOf("amazing-")>-1) {
+                    let amazingClass = el.className.split(" ").filter((v)=>v.indexOf("amazing-")>-1)[0].replace("amazing-","");
+                    let allAfter = document.querySelectorAll(`[data-after="${amazingClass}"]`);
+                    allAfter.forEach((after_element) => {
+                        setAnimation(after_element);
+                    });
+                }
             },parseFloat(speed+delay)*1000);
             setTimeout(()=>{
                 el.classList.add(`animation-${speed}s`);
                 el.classList.add("animated");
                 el.classList.add(animation);
                 el.style.visibility = "visible";
-            },delay*1000)
+            },delay*1000);
         }
         
         function scroll() {
             // Everytime the page is scrolled
-          var all_elements = document.querySelectorAll(`.${self.config.useClass}:not(.finished)`);
+          let all_elements = document.querySelectorAll(`.${self.config.useClass}:not(.finished)`);
                 // Catch all elements with the chosen class
             all_elements.forEach((el)=>{
               if (isScrolledIntoView(el)) {
-               var animation = el.dataset.animation || self.config.defaultAnimation;
-               var delay = el.dataset.delay || 0;
-               var speed = el.dataset.speed || 1;
-               var after = el.dataset.after || false;
-               speed = self.config.defaultSpeed || speed;
+               let after = el.dataset.after || false;
                if (!after)
                 setAnimation(el);
               }
