f1aec2e9dc728d151b91e188789bd0299d613d0d
Nir Tzezana <nirtz89@gmail.com>
2019-03-29 19:18:20 +0300

    Demo changed and mobile support added

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,24 @@ const sass = require('gulp-sass');
 const minify = require('gulp-minify');
 
 gulp.task('sass', function () {
-    return gulp.src('style/*.scss')
+    return gulp.src(['style/*.scss'])
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./style/'));
+});
+
+gulp.task('demo', function () {
+    return gulp.src(['demo/*.scss'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./demo'));
   });
 
 gulp.task('stream', function() {
   gulp.start('sass');
-  return gulp.watch(['style/*.scss'], function() {
+  gulp.start('compress');
+  return gulp.watch(['style/*.scss','demo/*.scss','src/script.js'], function() {
       gulp.start('sass');
+      gulp.start('demo');
+      gulp.start('compress');
   });
 });
 
--- src/script.js
+++ src/script.js
@@ -7,14 +7,33 @@ class Amazing {
         this.config.defaultAnimation = this.config.defaultAnimation || false;
         this.config.defaultSpeed = this.config.defaultSpeed || false;
         this.config.defaultDelay = this.config.defaultDelay || 0;
+        this.config.allowMobile = (this.config.allowMobile == false) ? false : true;
         this.debug = debug;
     }
+
+    isMobile() { 
+        if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+        ){
+            return true;
+            }
+        else {
+            return false;
+            }
+    }
+       
 
     init() {
         let self = this;
         if (self.debug)
             // If in debug mode, state the library has initiated and the object has been created
             console.log("Amazing initiated");
+        scroll();
 
         function isScrolledIntoView(el) {
             // If elements is scrolled into view
@@ -25,19 +44,20 @@ class Amazing {
             return isVisible;
         }
 
-        function setAnimation(el) {
+        function setAnimation(el,delay_of_after_el = 0) {
             let animation = el.dataset.animation || self.config.defaultAnimation;
             let delay = parseFloat(el.dataset.delay) || self.config.defaultDelay;
             let speed = parseFloat(el.dataset.speed) || self.config.defaultSpeed;
+            let mobile = el.dataset.mobile || self.config.allowMobile;
+            if (mobile == "false" && self.isMobile())
+                return;
             setTimeout(()=>{
                 el.classList.add("finished");
                 // Find elements waiting for this to finish and activate animation on them
                 if (el.className.indexOf("amazing-")>-1) {
                     let amazingClass = el.className.split(" ").filter((v)=>v.indexOf("amazing-")>-1)[0].replace("amazing-","");
                     let allAfter = document.querySelectorAll(`[data-after="${amazingClass}"]`);
-                    allAfter.forEach((after_element) => {
-                        setAnimation(after_element);
-                    });
+                    allAfter.forEach(after_element => setAnimation(after_element, speed+delay));
                 }
             },parseFloat(speed+delay)*1000);
             setTimeout(()=>{
@@ -45,7 +65,7 @@ class Amazing {
                 el.classList.add("animated");
                 el.classList.add(animation);
                 el.style.visibility = "visible";
-            },delay*1000);
+            },(delay+delay_of_after_el)*1000);
         }
         
         function scroll() {
